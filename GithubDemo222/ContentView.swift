228dd4a8026d5859e04ab2ce5fc7a652e9b92126
BoltayevFeruz <boltayevferuz777@gmail.com>
2022-07-15 08:20:54 +0500

    Initial Commit

---
+++ GithubDemo222/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubDemo222
+//
+//  Created by Feruz Boltayev on 15.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubDemo222/GithubDemo222App.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemo222App.swift
+//  GithubDemo222
+//
+//  Created by Feruz Boltayev on 15.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemo222App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
